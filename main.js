a1869dd485ee9d0f940ab4d05b25c2e2c72e5f93
Murc96 <marc.schori96mail.com>
2023-06-07 09:39:04 +0200

    update to main.js and styles.css

--- main.js
+++ main.js
@@ -2,18 +2,27 @@
 
 const btn = document.querySelector("#reset");
 const container = document.querySelector("#drawpad");
-const size = 16 * 16;
+const size = 16;
 
 btn.addEventListener("click", () => {
   removeAllChildren(container);
-  const promptInput = prompt();
-  const newSize = promptInput * promptInput;
-  document.querySelector(".container").style.gridTemplateColumn;
-  createGrid(newSize);
+  const promptInput = parseInt(
+    prompt(
+      "Bitte geben Sie eine zahl zwischen 1 und 100 ein um die Grid größe zu bestimmen."
+    )
+  );
+  if (promptInput >= 0 && promptInput <= 100) {
+    createGrid(promptInput);
+  } else {
+    alert("Es wird eine Zahl zwischen 1 und 100 benötigt!");
+  }
 });
 
-function createGrid(size) {
-  for (let i = 0; i < size; i++) {
+function createGrid(sizeGrid) {
+  container.style.gridTemplateColumns = `repeat(${sizeGrid} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${sizeGrid} , 1fr)`;
+  let amount = sizeGrid * sizeGrid;
+  for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     container.appendChild(box);
     box.classList.add("box");
